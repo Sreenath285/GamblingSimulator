c4791794a26854968fb7931e9e3446103ef5ea81
Sreenath285 <sreenath.285@gmail.com>
2022-02-19 12:06:06 +0530

    calculating 20 days of total gambled amount

---
+++ GamblingSimulatorPgm/src/Gambler.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Gambler {
+
+    public static void main(String[] args) {
+
+        int initialStake, betEveryGame, noOfDays;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter initial stake amount : ");
+        initialStake = sc.nextInt();
+        System.out.println("Enter bet amount : ");
+        betEveryGame = sc.nextInt();
+        System.out.println("Enter number of days to gamble : ");
+        noOfDays = sc.nextInt();
+        System.out.println();
+
+        int maxWinDaily, minWInDaily;
+        int dailyTotal = initialStake;
+        int totalAmt = 0;
+        
+        for (int i = 1; i <= noOfDays; i++) {
+            maxWinDaily = dailyTotal + (initialStake / 2);
+            minWInDaily = dailyTotal - (initialStake / 2) ;
+            while (dailyTotal < maxWinDaily && dailyTotal > minWInDaily) {
+                double isWin = Math.floor(Math.random() * 10 % 2);
+                if (isWin == 1) {
+                    dailyTotal += betEveryGame;
+                } else {
+                    dailyTotal -= betEveryGame;
+                }
+            }
+            if (dailyTotal == maxWinDaily) {
+                System.out.println("Day : " + i + " Gambler won the game");
+            }
+            else {
+                System.out.println("Day : " + i + " Gambler lost the game");
+            }
+            totalAmt += dailyTotal;
+        }
+        System.out.println("Total amount : " + totalAmt);
+    }
+}
